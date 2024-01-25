052e39ab7eda558462f5f84847536ad550f54ec2
Yash Bhappa <ybhappa@gmail.com>
2024-01-25 20:30:51 +0530

    NotesApp: Supabase connection

---
+++ 06-NotesApp/composables/useSupabase.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_KEY =
+  "<SUPABASE_KEY>";
+const SUPABASE_URL = <SUPABASE_URL>;
+
+const useSupabase = () => {
+  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+  return {
+    supabase,
+  };
+};
+
+export default useSupabase;
